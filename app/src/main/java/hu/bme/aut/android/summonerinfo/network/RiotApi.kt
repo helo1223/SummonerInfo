1921357cbcdb4278dc3e3f1c249da9fc5f877f23
Hajnal Gábor Márk <hajnalgabormark@edu.bme.hu>
2021-05-10 08:45:48 +0200

    Match history item fix
    Item icons now don't try to download 0.png and it works propery this time!

--- app/src/main/java/hu/bme/aut/android/summonerinfo/network/RiotApi.kt
+++ app/src/main/java/hu/bme/aut/android/summonerinfo/network/RiotApi.kt
@@ -12,28 +12,28 @@ interface RiotApi {
 
     @GET("/lol/summoner/v4/summoners/by-name/{summonerName}")
     fun getSummonerByName(
-            @Path("summonerName") summonerName: String?,
-            @Query("api_key") api_key: String?
+            @Path("summonerName") summonerName: String,
+            @Query("api_key") api_key: String
 
-    ): Call<Profile>?
+    ): Call<Profile>
 
     @GET("/lol/league/v4/entries/by-summoner/{encryptedSummonerId}")
     fun getLeaguesByEncryptedSummonerId(
-            @Path("encryptedSummonerId") encryptedSummonerId: String?,
-            @Query("api_key") api_key: String?
-    ): Call<List<League>>?
+            @Path("encryptedSummonerId") encryptedSummonerId: String,
+            @Query("api_key") api_key: String
+    ): Call<List<League>>
 
     @GET("https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids")
     fun getMatchesByPuuid(
-            @Path("puuid") puuid: String?,
-            @Query("count") count: Int = 15,
-            @Query("api_key") api_key: String?
+            @Path("puuid") puuid: String,
+            @Query("count") count: Int,
+            @Query("api_key") api_key: String
 
     ): Call<List<String>>
 
     @GET("https://europe.api.riotgames.com/lol/match/v5/matches/{matchId}")
     fun getMatchByMatchId(
-            @Path("matchId") matchId: String?,
-            @Query("api_key") api_key: String?
-    ): Call<MatchDto>?
+            @Path("matchId") matchId: String,
+            @Query("api_key") api_key: String
+    ): Call<MatchDto>
 }
