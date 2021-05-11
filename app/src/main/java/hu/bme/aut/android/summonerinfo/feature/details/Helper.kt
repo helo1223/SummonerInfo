33887d996ed473a71f6911a8f8fe54f2fd46b109
Hajnal Gábor Márk <hajnalgabormark@edu.bme.hu>
2021-05-11 23:35:34 +0200

    Moved loadImage method to separate class

---
+++ app/src/main/java/hu/bme/aut/android/summonerinfo/feature/details/Helper.kt
@@ -0,0 +1,20 @@
+package hu.bme.aut.android.summonerinfo.feature.details
+
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+
+class Helper {
+
+    companion object{
+        var championNamesMap : HashMap<Int, String> = HashMap()
+
+        fun loadImage(fragment: Fragment, url: String, param: String, target: ImageView) {
+                Glide.with(fragment)
+                        .load("$url$param.png")
+                        .transition(DrawableTransitionOptions().crossFade())
+                        .into(target)
+        }
+    }
+}
