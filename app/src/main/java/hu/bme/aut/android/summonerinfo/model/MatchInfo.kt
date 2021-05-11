23d04ce58ffb11ecbfd843eb49265e22beb6a28b
Hajnal Gábor Márk <hajnalgabormark@edu.bme.hu>
2021-05-11 23:36:46 +0200

    Removed unnecessary variables

--- app/src/main/java/hu/bme/aut/android/summonerinfo/model/MatchInfo.kt
+++ app/src/main/java/hu/bme/aut/android/summonerinfo/model/MatchInfo.kt
@@ -2,14 +2,12 @@ package hu.bme.aut.android.summonerinfo.model
 
 class MatchInfo{
     var gameCreation: String? = null
-    var gameDuration: Int = 0
     var gameMode: String? = null
-    var mapId: Int = 0
-    var participants: List<MatchParticipant>? = null
+    var participants: ArrayList<MatchParticipant>? = null
 
     fun findPlayerIndex(summonerId: String): MatchParticipant {
         for (participant in participants!!) {
-            if(participant.summonerId == summonerId.toString()){
+            if(participant.summonerId == summonerId){
                 return participant
             }
         }
